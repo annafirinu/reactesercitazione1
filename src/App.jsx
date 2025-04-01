01edecde17cbb03cbe019b71e1f8ff01a67b6eba
annafirinu <anna.firinu2@outlook.it>
2025-04-01 16:15:57 +0200

    FinalVersion

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,22 @@ function App() {
         </a>
       </div>
       <h1>Esercitazione React 1</h1>
-      <ButtonComponent text="Avanti" />
+      <h2>Bottoni</h2>
+      <ButtonComponent text="Primo" />
+      <ButtonComponent text="Secondo" />
+      <ButtonComponent text="Terzo" />
+      <h2>Immagini</h2>
       <ImageComponent
         link="https://img.freepik.com/vettori-gratuito/giraffa-carina-con-gli-occhiali-caricaturistici-vector-icon-illustration-animal-nature-icon-isolated-flat_138676-13785.jpg"
         description="Giraffa"
+      />
+      <ImageComponent
+        link="https://m.media-amazon.com/images/I/613Il8TkZKL._AC_UF1000,1000_QL80_.jpg"
+        description="Panda"
+      />
+      <ImageComponent
+        link="https://www.balloon-party.it/68474-large_default/palloncino-30-super-shape-scimmia-sergio.jpg"
+        description="Scimmia"
       />
     </>
   );
